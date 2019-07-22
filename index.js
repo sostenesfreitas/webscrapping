0251ff1abb96b7ef0386fe989f7b25922cebd23f
sostenes <sostenesfreitas@gmai.com>
2019-07-22 07:43:18 -0300

    vai

---
+++ index.js
@@ -0,0 +1,25 @@
+const puppeteerInfiniteScroll = require('./node_modules/puppeteer-infinite-scroll/src/puppeter-infinite-scroll')
+const skel = require('./skel')
+let t = async () => {
+	const browser = new puppeteerInfiniteScroll()
+	await browser.start({ headless: true })
+	await browser.open({
+		url: 'https://www.ofertaesperta.com/',
+		endpoint: 'https://www.ofertaesperta.com/',
+		loadImages: false,
+		onScroll: () => {
+			test = browser.stract(skel)
+		}
+	})
+	browser.scrollFreeze()
+	browser.close()
+	return await test
+}
+
+t()
+	.then(a => {
+		console.log(a)
+	})
+	.catch(err => {
+		console.log(err)
+	})
